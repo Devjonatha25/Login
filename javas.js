a0c684eae0b8d82c7798bda6b5e1f95c1a3aedbb
Devjonatha25 <123656886+Devjonatha25@users.noreply.github.com>
2023-05-16 22:46:00 -0300

    enviar

---
+++ javas.js
@@ -0,0 +1,18 @@
+const btn1  = document.querySelector('.btn')
+const dark = document.querySelector('.dark')
+const are1 = document.querySelector('.are')
+
+btn1.onclick = function(){
+ 
+
+  this.classList.toggle('active')
+  dark.classList.toggle('active')
+  are1.classList.toggle('active')
+ 
+ 
+}
+
+
+
+  
+
